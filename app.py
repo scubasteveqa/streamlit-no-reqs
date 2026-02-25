10f11615a3ecb88d3be0f07104a5f7508083f8d7
scubasteveqa <154364741+scubasteveqa@users.noreply.github.com>
2026-02-25 12:39:42 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from streamlit_player import st_player
 
 # Embed a youtube video Rick Roll
-st_player("https://www.youtube.com/watch?v=eBGIQ7ZuuiU")
+ st_player("https://www.youtube.com/watch?v=eBGIQ7ZuuiU", playing=True)    
 
 # Embed a youtube video - Alan Walker
 #st_player("https://www.youtube.com/watch?v=wJnBTPUQS5A")
